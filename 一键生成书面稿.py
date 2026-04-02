ab196193c9c6cdd344703f1e2081269873bbd6a6
ding_mie <ding_mie@163.com>
2026-04-02 17:04:49 +0800

    chore: 更新音频和文本路径为摩诃止观009

--- 一键生成书面稿.py
+++ 一键生成书面稿.py
@@ -144,8 +144,8 @@ class AudioProcessingPipeline:
 
 if __name__ == "__main__":
     # ---------------- 基础配置 ----------------
-    audio_path = "摩诃止观-久仁法师/摩诃止观008/摩诃止观008.mp3"
-    fuzzy_text_path = "摩诃止观-久仁法师/摩诃止观008/008原文模糊范围.txt"
+    audio_path = "摩诃止观-久仁法师/摩诃止观009/摩诃止观009.mp3"
+    fuzzy_text_path = "摩诃止观-久仁法师/摩诃止观009/009原文模糊范围.txt"
 
     # ---------------- 初始化引擎 ----------------
     print(f"{Color.DARK_PURPLE}正在初始化 Qwen3ASRFlashFiletrans 引擎...")
